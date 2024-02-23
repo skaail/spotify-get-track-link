3c25ace0b3b2ca924f0755c366fe4e8422f89b3d
Bruno Gianini <blgianini@gmail.com>
2024-02-23 02:48:06 -0300

    return to basic

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
-
-function getSpotifyAuthToken(clientId, clientSecret) {
-    return new Promise((resolve, reject) => {
-      const authString = `${clientId}:${clientSecret}`;
-      const encodedAuthString = Buffer.from(authString).toString('base64');
+function add(a, b) {
+    return a + b;
+  }
   
-      const requestOptions = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Authorization': `Basic ${encodedAuthString}`
-        },
-        body: 'grant_type=client_credentials'
-      };
-  
-      fetch('https://accounts.spotify.com/api/token', requestOptions)
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error));
-    });
-}
-
-module.exports = getSpotifyAuthToken;
+  module.exports = add;
